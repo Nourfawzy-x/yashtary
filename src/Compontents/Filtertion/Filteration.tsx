05a6db91a1015e0e4b32cf8ef046563806f1c2ed
nour fawzy <nourfawzy292001@gmail.com>
2024-06-12 06:11:07 +0300

    finish filteration section (desktop and mobile screen)

--- src/Compontents/Filtertion/Filteration.tsx
+++ src/Compontents/Filtertion/Filteration.tsx
@@ -18,119 +18,71 @@ export default function Filteration() {
         </div>
       </div>
 
-      <div className="mobile-filteration mt-3 container  end-0">
-        <div
-          className="modal fade"
-          id="exampleModalToggle"
-          aria-hidden="true"
-          aria-labelledby="exampleModalToggleLabel"
-          tabindex="-1"
-        >
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalToggleLabel">
-                  filter
-                </h1>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body d-flex flex-column justify-content-center align-items-center">
-                <div className="btn-group w-100 my-4">
+      <div className={` mt-3  py-3 container ${style.mobile_filteration}`}>
+        <div className="d-flex justify-content-end">
+          <div
+            className="modal fade"
+            id="exampleModalToggle"
+            aria-hidden="true"
+            aria-labelledby="exampleModalToggleLabel"
+            tabindex="-1"
+          >
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalToggleLabel">
+                    filter
+                  </h1>
                   <button
                     type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Men
-                  </button>
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
                 </div>
-                <div className="btn-group w-100 mb-4">
+                <div className="modal-body d-flex flex-column justify-content-center align-items-center">
+                  {[
+                    "Men",
+                    "Women",
+                    "Unisex",
+                    "Kids",
+                    "Best Sellers",
+                    "New Arrivals",
+                    "Offers",
+                  ].map((item) => (
+                    <div className="btn-group w-100 mb-4" key={item}>
+                      <button
+                        type="button"
+                        className="btn dropdown-toggle px-5 bg-white"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        {item}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+                <div className="modal-footer">
                   <button
-                    type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+                    className="btn bg-black text-white border-2"
+                    data-bs-target="#exampleModalToggle2"
+                    data-bs-toggle="modal"
                   >
-                    Women
+                    save
                   </button>
                 </div>
-                <div className="btn-group w-100 mb-4">
-                  <button
-                    type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Unisex
-                  </button>
-                </div>
-                <div className="btn-group w-100 mb-4">
-                  <button
-                    type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Kids
-                  </button>
-                </div>
-                <div className="btn-group w-100 mb-4">
-                  <button
-                    type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Best Sellers
-                  </button>
-                </div>
-                <div className="btn-group w-100 mb-4">
-                  <button
-                    type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    New Arrivals
-                  </button>
-                </div>
-                <div className="btn-group w-100 mb-4">
-                  <button
-                    type="button"
-                    className="btn dropdown-toggle px-5 bg-white"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Offers
-                  </button>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button
-                  className="btn bg-black text-white border-2"
-                  data-bs-target="#exampleModalToggle2"
-                  data-bs-toggle="modal"
-                >
-                  save
-                </button>
               </div>
             </div>
           </div>
-        </div>
 
-        <button
-          className="filter-btn bg-transparent text-black rounded-2 py-1 px-5 "
-          data-bs-target="#exampleModalToggle"
-          data-bs-toggle="modal"
-        >
-          filtertion
-        </button>
+          <button
+            className="filter-btn bg-transparent text-black rounded-2 py-1 px-5 "
+            data-bs-target="#exampleModalToggle"
+            data-bs-toggle="modal"
+          >
+            filtertion
+          </button>
+        </div>
       </div>
     </>
   );
